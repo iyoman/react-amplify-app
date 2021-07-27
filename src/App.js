c917c0f926cd67aaf78611457d49f238807ceb5d
Ike Banana <isaac.p.avery@gmail.com>
2021-07-27 12:01:55 -0400

    the best one

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import './App.css';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+Auth.currentAuthenticatedUser({
+  bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+}).then(user => console.log(user))
+  .catch(err => console.log(err))
+
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Hexatank!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AmplifySignOut />
-    </div>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome to Hexatank!
+      </p>
+      <p id="printuser"></p>
+    </header>
+  </div>
 );
+
+window.onload = function(){
+  document.getElementById('printuser').innerHTML = user;
+};
 
 export default withAuthenticator(App);
