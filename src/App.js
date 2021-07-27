6f8c0b5aae9bd0fa784dfdb5de7b3fd902b9b9e1
iyoman <isaac.p.avery@gmail.com>
2021-07-27 17:26:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 
 function App() {
@@ -13,25 +11,22 @@ function App() {
     const user = await Auth.currentAuthenticatedUser();
     console.log("user: ", user)
   }
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Welcome to Hexatank!
-      </p>
-      <button onClick={checkUser}>
-        Check the current user
-      </button>
-      <p id="printuser"></p>
-    </header>
-    <p id="nothead">not a header</p>
-  </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Hexatank!
+        </p>
+        <button onClick={checkUser}>
+          Check the current user
+        </button>
+        <p id="printuser"></p>
+      </header>
+      <p id="nothead">not a header</p>
+    </div>
+  )
+
 }
-  
 
-
-window.onload = function(){
-  document.getElementById('printuser').innerHTML = "not done yet";
-};
-
-export default withAuthenticator(App,true);
+export default withAuthenticator(App, true);
