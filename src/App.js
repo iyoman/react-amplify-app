72f0344b49af257db0533df431660af74bf3cf00
iyoman <isaac.p.avery@gmail.com>
2021-07-27 13:14:41 -0400

    Update App.js
    change to function

--- src/App.js
+++ src/App.js
@@ -4,29 +4,33 @@ import './App.css';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { Auth } from 'aws-amplify';
 
-Auth.currentAuthenticatedUser({
-  bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-}).then(user => console.log(user))
-.catch(err => console.log(err));
 
-const App = () => (
+function App() {
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log("user: ", user)
+  }
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Welcome to Hexatank!
       </p>
+      <button onClick={checkUser}>
+        Check the current user
+      </button>
       <p id="printuser"></p>
     </header>
+    <p id="nothead">not a header</p>
   </div>
-);
+}
+  
+
 
 window.onload = function(){
   document.getElementById('printuser').innerHTML = "not done yet";
 };
 
-export default withAuthenticator(App);
+export default withAuthenticator(App,true);
