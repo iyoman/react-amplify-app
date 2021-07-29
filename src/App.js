82028b66431d208d7cd9b0c9904fe6768b8a6fdf
iyoman <isaac.p.avery@gmail.com>
2021-07-29 11:30:35 -0400

    add file storage

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ import { Hub, Logger } from 'aws-amplify';
 const logger = new Logger('My-Logger');
 
 const listener = (data) => {
-    switch (data.payload.event) {
-        case 'signIn':
-            logger.info('user signed in');
-            break;
-        case 'signUp':
-            logger.info('user signed up');
-            break;
-        case 'signOut':
-            logger.info('user signed out');
-            break;
-        case 'signIn_failure':
-            logger.error('user sign in failed');
-            break;
-        case 'tokenRefresh':
-            logger.info('token refresh succeeded');
-            break;
-        case 'tokenRefresh_failure':
-            logger.error('token refresh failed');
-            break;
-        case 'configured':
-            logger.info('the Auth module is configured');
-    }
+  switch (data.payload.event) {
+    case 'signIn':
+      logger.info('user signed in');
+      break;
+    case 'signUp':
+      logger.info('user signed up');
+      break;
+    case 'signOut':
+      logger.info('user signed out');
+      break;
+    case 'signIn_failure':
+      logger.error('user sign in failed');
+      break;
+    case 'tokenRefresh':
+      logger.info('token refresh succeeded');
+      break;
+    case 'tokenRefresh_failure':
+      logger.error('token refresh failed');
+      break;
+    case 'configured':
+      logger.info('the Auth module is configured');
+  }
 }
 
 function App() {
@@ -40,6 +40,17 @@ function App() {
     const user = await Auth.currentAuthenticatedUser();
     console.log("user: ", user)
     document.getElementById("printuser").innerHTML = user["attributes"]["email"]
+  }
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      await Storage.put('test.txt', 'Protected Content by Isaac', {
+        level: 'protected',
+        contentType: 'text/plain'
+      });
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+    }
   }
   return (
     <div className="App">
@@ -52,6 +63,10 @@ function App() {
           Check the current user
         </button>
         <p id="printuser"></p>
+        <input
+          type="file"
+          onChange={onChange}
+        />
       </header>
       <AmplifySignOut />
     </div>
